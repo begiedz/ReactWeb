2d6f5402808eee83f8ba1b5d24aefa56ebb4a0ff
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2023-06-14 01:34:41 +0200

    Update styles to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,22 +3,24 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-            <div>© 2023 Dariusz Begiedza. All rights reserved.</div>
-            <div>Made with React. The library for web and native user interfaces.</div>
+            <div className="footerWrapper">
+                <div>© 2023 Dariusz Begiedza. All rights reserved.</div>
+                <div>Made with React. The library for web and native user interfaces.</div>
+            </div>
 
             <ul>
                 Contact
                 <li>
                     Phone:
-                    <a href="tel:+48782041173">+48 782 041 173</a>
+                    <a href="tel:+48782041173"> +48 782 041 173</a>
                 </li>
                 <li>
                     Discord:
-                    <a href="https://discordapp.com/users/689833947995046039">begiedz#5194</a>
+                    <a href="https://discordapp.com/users/689833947995046039"> begiedz#5194</a>
                 </li>
                 <li>
                     e-mail:
-                    <a href="mailto:begiedz.developer@gmail.com">begiedz.developer@gmail.com</a>
+                    <a href="mailto:begiedz.developer@gmail.com"> begiedz.developer@gmail.com</a>
                 </li>
             </ul>
         </footer>
