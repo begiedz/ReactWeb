b625e792ed04b504536044bf996f48a2c44311ef
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2023-06-22 23:57:17 +0200

    Add basic HTML form template

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const Contact = () => {
-    return <div>Contact</div>;
+    return (
+        <div className="contact">
+            Contact
+            <form action="">
+                <label htmlFor="nameInput">Name</label>
+                <input type="text" id="nameInput" />
+                <label htmlFor="emailInput">Email</label>
+                <input type="email" id="emailInput" />
+                <label htmlFor="title">Title</label>
+                <input type="text" id="title" />
+                <label htmlFor="textarea">Message</label>
+                <textarea id="textarea" />
+                <input type="submit" />
+            </form>
+        </div>
+    );
 };
 
 export default Contact;
