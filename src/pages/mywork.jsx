fef6b93880dd8d9d43764fef8ea6fa0a82e86e50
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2023-07-30 21:09:16 +0200

    add json data fetching

--- src/pages/mywork.jsx
+++ src/pages/mywork.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import projects from '../json/projects.json'
+
 const MyWork = () => {
+    console.log(projects);
+
     return (
         <div className="gallery">
             <h2> My work</h2>
@@ -8,15 +12,27 @@ const MyWork = () => {
                 <button>JS</button>
                 <button>PHP</button>
                 <button>React</button>
-                <button>React Native</button>
+                <button>Node.js</button>
             </div>
             <div className="output">
+                {/* <div className="item">
+                    <img src="" alt="" />
+                    <p>{projects[0].}</p>
+                    <a href="">github</a>
+                    <a href="">project</a>
+                </div>
                 <div className="item">item</div>
                 <div className="item">item</div>
                 <div className="item">item</div>
                 <div className="item">item</div>
-                <div className="item">item</div>
-                <div className="item">item</div>
+                <div className="item">item</div> */}
+                {projects.map(project => {
+                    return (<div className='item'>
+                        <img src={project.imgPath} alt="" />
+                        <p>{project.appName}</p>
+                    </div>
+                    )
+                })}
             </div>
         </div>
     );
--- src/pages/skills.jsx
+++ src/pages/skills.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
 const Skills = () => {
-    return <div>Skills</div>;
+
+    const skillsList = [
+        { title: 'HTML', description: 'The basics of HTML I have learned at 1 year course' },
+        { title: 'CSS', description: 'lorem ipsum 2' },
+        { title: 'JavaScript', description: 'lorem ipsum 3' },
+        { title: 'React', description: 'lorem ipsum 4' },
+    ];
+
+
+    return (
+        <div className='skills'>
+            <h2>Skills</h2>
+            <ul>
+                {skillsList.map(skill => {
+                    return (
+                        <li>
+                            <h3>{skill.title}</h3>
+                            <p>{skill.description}</p>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    );
 };
 
 export default Skills;
