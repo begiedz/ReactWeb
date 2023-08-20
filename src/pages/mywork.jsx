8d1b0170b32a771e739c9b29ca09fc49db934a20
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2023-08-20 14:49:48 +0200

    add propper json img rendering

--- src/pages/mywork.jsx
+++ src/pages/mywork.jsx
@@ -3,15 +3,18 @@ import projects from '../json/projects.json'
 import { Link } from 'react-router-dom';
 import { FaGithub } from 'react-icons/fa';
 
-
 const MyWork = () => {
     console.log(projects);
 
+    const handleInput = () => {
+
+    }
     return (
         <div className="gallery">
-            <h2> My work</h2>
+            <h2>My work</h2>
+
             <div className='searchUI'>
-                <input type="search" placeholder='Search...' />
+                <input type="search" placeholder='Search...' onInput={handleInput()} />
                 <div className="buttonWrapper">
                     <button>All</button>
                     <button>JS</button>
@@ -21,18 +24,24 @@ const MyWork = () => {
                 </div>
             </div>
             <div className="output">
-                {projects.map(project => {
-                    return (<div className='item'>
-                        <img src={project.imgPath} alt={project.appName} />
-                        <p>{project.appName}</p>
-                        <ul>
-                            <li>
-                                <a href={project.githubURL}><FaGithub /></a></li>
-                            {project.previewURL ? <li><Link to={project.previewURL}>Preview</Link></li> : null}
-                        </ul>
-                    </div>
-                    )
-                })}
+                {
+                    projects &&
+                    projects.map(project => {
+                        return (<div className='item'>
+                            <img src={project.imgPath} alt={project.appName} />
+                            <p>{project.appName}</p>
+                            <ul>
+                                <li>
+                                    <a href={project.githubURL}><FaGithub /></a></li>
+                                {project.previewURL && <li><Link to={project.previewURL}>Preview</Link></li>}
+                            </ul>
+                        </div>
+                        )
+                    })
+
+                }
+
+
             </div>
         </div>
     );
