89abf454696ffa494ebbc6366ef7324c9b69a38e
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2023-08-23 12:11:34 +0200

    Add images and language list to gallery

--- src/pages/mywork.jsx
+++ src/pages/mywork.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import projects from '../json/projects.json'
 import { Link } from 'react-router-dom';
-import { FaGithub } from 'react-icons/fa';
+// import { FaGithub } from 'react-icons/fa';
 
 const MyWork = () => {
     console.log(projects);
@@ -28,18 +28,20 @@ const MyWork = () => {
                     projects &&
                     projects.map(project => {
                         return (
-                            <div className='card'>
-                                <a href={project.githubURL}>
+                            <a href={project.githubURL}>
+                                <div className='card'>
                                     <img src={project.imgPath} alt={project.appName} />
                                     <h3>{project.appName}</h3>
                                     <p>{project.description}</p>
                                     <ul>
-                                        <li>
-                                            {project.previewURL && <li><Link to={project.previewURL}>Preview</Link></li>}
-                                        </li>
+                                        {project.language.map(language => {
+                                            return (
+                                                <li>{language}</li>
+                                            )
+                                        })}
                                     </ul>
-                                </a>
-                            </div>
+                                </div>
+                            </a>
                         )
                     })
 
