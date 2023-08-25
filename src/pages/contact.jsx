5751b8398e1a5e75d274ca27e0a0b38e356f4fd1
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2023-08-25 12:41:40 +0200

    code refubrication

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,30 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const Contact = () => {
+
     return (
         <div className="contact">
             <h2>Contact</h2>
-            <form action="">
-                {/* <div className="inputWrapper">
-                    <label htmlFor="nameInput">Name:</label>
-                    <input type="text" id="nameInput" />
-                </div>
-                <div className="inputWrapper">
-                    <label htmlFor="emailInput">Email:</label>
-                    <input type="email" id="emailInput" />
-                </div>
 
-                <div className="inputWrapper">
-                    <label htmlFor="title">Title:</label>
-                    <input type="text" id="title" />
-                </div> */}
-
-
+            <form onSubmit={sendEmail()}>
                 <input type="text" id="nameInput" placeholder='Your name' />
                 <input type="email" id="emailInput" placeholder='Your email' />
                 <input type="text" id="title" placeholder='Message Title' />
                 <textarea id="textarea" placeholder='Message' />
-
                 <input type="submit" />
             </form>
         </div>
