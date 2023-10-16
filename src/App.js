05b0c84235c3a33e779c0b31cb663d415b7c986d
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2023-10-16 21:44:50 +0200

    change scss variables, start work on darkmode

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState, createContext } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
@@ -11,21 +12,31 @@ import Skills from './pages/Skills';
 import MyWork from './pages/MyWork';
 import Contact from './pages/Contact';
 
+export const ThemeContext = createContext(null);
+// const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
 function App() {
+  const [theme, setTheme] = useState('');
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === 'light' ? 'dark' : 'light'));
+  };
   return (
-    <div className="app light">
-      <I18nextProvider i18n={i18n}>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/mywork" element={<MyWork />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Footer />
-      </I18nextProvider>
-    </div>
+    <I18nextProvider i18n={i18n}>
+      <ThemeContext.Provider value={{ theme, setTheme }}>
+        <div className={`app ${theme}`}>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/mywork" element={<MyWork />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+          <Footer />
+        </div>
+      </ThemeContext.Provider>
+    </I18nextProvider>
   );
 }
 
