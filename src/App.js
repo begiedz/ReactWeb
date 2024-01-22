d90795de26b212fd4cde60e28834256526d0ccee
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2024-01-22 15:00:39 +0100

    Change dark mode application to scss prefers-color-scheme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
@@ -13,16 +12,9 @@ import MyWork from './pages/MyWork';
 import Contact from './pages/Contact';
 
 function App() {
-  const [theme, setTheme] = useState('');
-
-  useEffect(() => {
-    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    setTheme(prefersDark ? 'dark' : 'light');
-  }, []);
-
   return (
     <I18nextProvider i18n={i18n}>
-      <div className={`app ${theme}`}>
+      <div className="app">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
