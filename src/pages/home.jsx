0da56b5415f21fecd1bed91ec9f2a968751c8535
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2024-03-10 16:51:26 +0100

    update logo underlay to pattern

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,10 +9,8 @@ const Home = () => {
     return (
         <header>
             <div className="underlay reveal" style={{ animationDelay: 200 + 'ms' }}>
-                <svg id="bgdzLogo" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 177.82 250">
-                    <path d="M172.5,162.26l-82.43,82.42c-7.13,7.13-18.71,7.09-25.8-.09l-27.26-27.64-31.7-31.7C1.91,181.85,0,177.23,0,172.42V0H.21L39.92,39.71c3.41,3.41,5.32,8.03,5.32,12.85V208.72l46.43-46.42c7.12-7.12,7.1-18.66-.04-25.76l-31.77-31.41,27.29-27.67c7.08-7.17,18.64-7.23,25.78-.12l59.54,59.18c7.13,7.1,7.14,18.63,.03,25.74Z" />
-                </svg>
             </div>
+
             <div className="boxShadow"></div>
             <div className="titleWrapper">
                 <div className="hello reveal" style={{ animationDelay: 200 + 'ms' }} >{t('header.hello')}</div>
