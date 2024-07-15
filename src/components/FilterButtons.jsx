aedf063c9467d76829ce68a0ac5ce4e1c3dc1af1
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2024-07-15 21:26:31 +0200

    translation update

--- src/components/FilterButtons.jsx
+++ src/components/FilterButtons.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 // List of available filter button technologies
 const technologies = ['all', 'js', 'ts', 'react', 'react native', 'node.js', 'php'];
 
 const FilterButtons = ({ selectedTechnology, onFilterChange }) => {
+  const { t } = useTranslation();
   return (
     <div className="buttonWrapper">
       {technologies.map((tech) => (
@@ -12,9 +14,11 @@ const FilterButtons = ({ selectedTechnology, onFilterChange }) => {
           onClick={() => onFilterChange(tech)}
           className={tech === selectedTechnology ? 'active' : ''}
         >
-          {tech.includes(' ') ? tech.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ') :
-            (tech.length === 2 || tech === 'php' ? tech.toUpperCase() :
-              tech[0].toUpperCase() + tech.substring(1))}
+          {tech === 'all' ? t('projects.all') :
+            tech.includes(' ') ? tech.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ') :
+              (tech.length === 2 || tech === 'php' ? tech.toUpperCase() :
+                tech[0].toUpperCase() + tech.substring(1))
+          }
         </button>
       ))}
     </div>
