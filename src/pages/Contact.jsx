b59f4e20070b9c698a87d68850dd5b295fd80a57
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2025-03-14 20:13:05 +0100

    update contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,27 +8,27 @@ import { MdDone } from "react-icons/md";
 
 const Contact = () => {
     const [showNotification, setShowNotification] = useState(false)
-    const [message, setMessage] = useState('')
-    const [errorMessage, setErrorMessage] = useState('');
-    const [userName, setUserName] = useState('');
-    const [userEmail, setUserEmail] = useState('');
+    // const [message, setMessage] = useState('')
+    // const [errorMessage, setErrorMessage] = useState('');
+    // const [userName, setUserName] = useState('');
+    // const [userEmail, setUserEmail] = useState('');
     const form = useRef();
 
     const sendEmail = (e) => {
         e.preventDefault();
 
-        if (userName.length === 0 || userName === '') {
-            setErrorMessage('Name should not be empty.')
-            return;
-        }
-        if (userEmail.length === 0 || userEmail === '') {
-            setErrorMessage('Email should not be empty.')
-            return;
-        }
-        if (message.length < 5 || message === '') {
-            setErrorMessage('Message should have at least 5 characters.')
-            return;
-        }
+        // if (userName.length === 0 || userName === '') {
+        //     setErrorMessage('Name should not be empty.')
+        //     return;
+        // }
+        // if (userEmail.length === 0 || userEmail === '') {
+        //     setErrorMessage('Email should not be empty.')
+        //     return;
+        // }
+        // if (message.length < 5 || message === '') {
+        //     setErrorMessage('Message should have at least 5 characters.')
+        //     return;
+        // }
 
         emailjs.sendForm(
             process.env.REACT_APP_YOUR_SERVICE_ID,
@@ -77,9 +77,9 @@ const Contact = () => {
                                 key={index}
                                 name={field.name}
                                 placeholder={t(field.placeholder)}
-                                onChange={(e) => {
-                                    setMessage(e.target.value);
-                                }}
+                                // onChange={(e) => {
+                                // setMessage(e.target.value);
+                                // }}
                                 className="reveal"
                                 style={{ animationDelay: index * 200 + "ms" }}
                             />)
@@ -92,10 +92,10 @@ const Contact = () => {
                                 name={field.name && field.name}
                                 placeholder={field.placeholder && t(field.placeholder)}
                                 value={field.value && t(field.value)}
-                                onChange={(e) => {
-                                    if (field.name === "user_name") setUserName(e.target.value)
-                                    else if (field.name === "user_email") setUserName(e.target.value)
-                                }}
+                                // onChange={(e) => {
+                                //     if (field.name === "user_name") setUserName(e.target.value)
+                                //     else if (field.name === "user_email") setUserEmail(e.target.value)
+                                // }}
                                 className="reveal"
                                 style={{ animationDelay: index * 200 + "ms" }} />)
                     }
